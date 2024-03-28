0a9699f2c5455a78b24e830cdc9252480e896b3f
d-miyabi <msgh3110+works@gmail.com>
2024-03-29 06:17:46 +0900

    本人のことにパスを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+    redirect_to '/'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:personal_info, :funeral)
+  end
 end
