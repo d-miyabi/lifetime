cd9ed6f19fd970262c733b1b3e283c3c79962c9c
d-miyabi <msgh3110+works@gmail.com>
2024-04-02 08:35:14 +0900

    form_selectの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         enum funeral: { する: 0, しない: 1 }
+         enum funeral_wishes: { する: 0, しない: 1 }
+         enum funeral_form: { 一般葬: 0, 家族葬: 1, 喪主に任せる: 2}
 end
