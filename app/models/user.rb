25469f878cf6935581e8f6184f889a15e44697c2
d-miyabi <msgh3110+works@gmail.com>
2024-04-19 08:25:01 +0900

    modelにbelongingsのenumを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
          enum funeral_form: { 一般葬: 0, 家族葬: 1, 喪主に任せる: 2 }
          enum ashes: { 墓: 0, 納骨堂: 1, 自宅:2, 散骨する:3, その他:4 }
          enum posthuomus_name: { つける: 0, つけない: 1 }
+         enum belongings: { 処分する: 0, 遺族に任せる: 1}
 
   has_many :emotions
   has_many :friends
