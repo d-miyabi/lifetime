148b3451198940d57b496c010e3b9feeb0653b38
d-miyabi <msgh3110+works@gmail.com>
2024-04-21 07:26:23 +0900

    friendsのマイグレーションファイルを修正

--- db/migrate/20240321225440_create_friends.rb
+++ db/migrate/20240321225440_create_friends.rb
@@ -2,7 +2,7 @@ class CreateFriends < ActiveRecord::Migration[7.0]
   def change
     create_table :friends do |t|
       t.string     :friends_name
-      t.string     :email
+      t.string     :contact
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_09_222224) do
 
   create_table "friends", charset: "utf8", force: :cascade do |t|
     t.string "friends_name"
-    t.string "email"
+    t.string "contact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
