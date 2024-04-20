4608c8226576581400e3e52cd8cd773395cb2939
d-miyabi <msgh3110+works@gmail.com>
2024-04-21 08:29:46 +0900

    friendのcontrollerにアクションを定義

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,26 @@
 class FriendsController < ApplicationController
+
+  def new
+    @friend = Friend.new
+  end
+
+  def show
+  end
+
+  def create
+    @friend = current_user.friends.build(friend_params)
+    if @friend.save
+      redirect_to root_path
+    else
+    @errors = @friend.errors.full_messages
+    render:new
+    end
+  end
+
+  private
+  
+  def friend_params
+    params.require(:friend).permit( :friend_name, :contact)
+  end
+
 end
