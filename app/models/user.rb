4fa42e709477602431e0427ed0c8eed828ac1789
d-miyabi <msgh3110+works@gmail.com>
2024-04-26 08:34:09 +0900

    ja.ymlに遺骨を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          
   enum funeral_wishes:  { do: 0, do_not: 1 }
   enum funeral_form:    { general_funeral: 0, family_funeral: 1, mourner: 2 }
-  enum ashes:           { 墓: 0, 納骨堂: 1, 自宅:2, 散骨する:3, その他:4 }
+  enum ashes:           { grave: 0, ossuary: 1, home:2, scattered_bones:3, others:4 }
   enum posthuomus_name: { つける: 0, つけない: 1 }
   enum belongings:      { 処分する: 0, 遺族に任せる: 1}
 
