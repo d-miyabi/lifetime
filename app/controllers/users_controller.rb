c4a6aca499d993102364deef2ece5f4ae408bd42
d-miyabi <msgh3110+works@gmail.com>
2024-04-25 19:56:55 +0900

    マイページへのリンクを設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,8 @@ before_action :move_to_index, except: [:index, :show]
   private
   
   def user_params
-    params.require(:user).permit(:strengths, :hobbies, :skills, :funeral_wishes, :funeral_form, :posthumous_name_budget, :funeral_home_budget, :reword_budget, :others_budget, :chief_mourner, :kinship, :ashes, :posthuomus_name, :belongings, :legacy, :emotion)
+    params.require(:user).permit(:strengths, :hobbies, :skills, 
+    :funeral_wishes, :funeral_form, :posthumous_name_budget, :funeral_home_budget, :reword_budget, :others_budget, :chief_mourner, :kinship, :ashes, :posthuomus_name, :belongings, :legacy, :emotion)
   end
 
   def move_to_index
