81158b26083e4df8cd80ecbbd5094a2f34760513
d-miyabi <msgh3110+works@gmail.com>
2024-04-26 08:16:46 +0900

    funeral_formの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   enum funeral_wishes:  { do: 0, do_not: 1 }
-  enum funeral_form:    { 一般葬: 0, 家族葬: 1, 喪主に任せる: 2 }
+  enum funeral_form:    { general_funeral: 0, family_funeral: 1, mourner: 2 }
   enum ashes:           { 墓: 0, 納骨堂: 1, 自宅:2, 散骨する:3, その他:4 }
   enum posthuomus_name: { つける: 0, つけない: 1 }
   enum belongings:      { 処分する: 0, 遺族に任せる: 1}
