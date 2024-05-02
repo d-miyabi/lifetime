e7e288004bb06e58894e8b88fc572f7c71d4212b
d-miyabi <msgh3110+works@gmail.com>
2024-05-02 19:17:18 +0900

    javascriptの記述を修正

--- app/javascript/friends.js
+++ app/javascript/friends.js
@@ -1,16 +1,21 @@
 function add_form_element() {
-  // Select the form
-  var form = document.getElementsByTagName('form')[0];
+  const form = document.querySelector('.personal form');
 
-  // Create new input element
-  var newInput = document.createElement('input');
-  newInput.setAttribute('type', 'text');
-  newInput.setAttribute('name', 'newInput');
+  // Create new input elements
+  const newInput1 = document.createElement('input');
+  newInput1.setAttribute('type', 'text');
+  newInput1.setAttribute('placeholder', '友人・知人を入力しましょう');
 
-  // Create a line break
-  var lineBreak = document.createElement('br');
+  const newInput2 = document.createElement('input');
+  newInput2.setAttribute('type', 'text');
+  newInput2.setAttribute('placeholder', '連絡方法を入力しましょう');
 
-  // Append the new input field and line break to the form
-  form.insertBefore(newInput, form.childNodes[0]);
-  form.insertBefore(lineBreak, form.childNodes[1]);
+  // Append the new input fields to the form 
+  form.appendChild(newInput1);
+  form.appendChild(newInput2);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector(".add-btn").addEventListener("click", add_form_element);
+});
+
